f87ac5371f0554f9c877ec309e6e1ca5fecec145
Stanislav_Kuranov <kuranovstas18@yandex.ru>
2023-06-21 11:27:32 +0500

    исправление middleware

--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -10,9 +10,8 @@ type TWsActions = {
   onError: string,
   getOrders: string
 }
-// не особо понимаю эту типизацию -_-
+
 export const socketMiddleware = (wsUrl: string, wsActions: TWsActions, auth: boolean): Middleware<{}, RootState> => {
- // через store: MiddlewareAPI<AppDispatch, RootState> не работало, наставник подсказал типизировать Middleware<{}, RootState> 
   return (store) => {
     let socket: WebSocket | null = null;
 
@@ -22,39 +21,38 @@ export const socketMiddleware = (wsUrl: string, wsActions: TWsActions, auth: boo
       const { wsStart, onOpen, onClose, onError, getOrders } = wsActions;
 
       const accessToken = getCookie('accessToken');
+
       if (type === wsStart && auth === false) {
         socket = new WebSocket(wsUrl);
       } else if (type === wsStart && auth === true && accessToken) {
         const accessToken = getCookie("accessToken");
-        socket = new WebSocket(
-          `${wsUrl}?token=${accessToken}`
-        );
+        socket = new WebSocket(`${wsUrl}?token=${accessToken}`);
       }
 
       if (socket) {
-        // функция, которая вызывается при открытии сокета
         socket.onopen = (event: Event) => {
           dispatch({ type: onOpen, payload: event });
         };
 
-        // функция, которая вызывается при ошибке соединения
         socket.onerror = (event: Event) => {
           dispatch({ type: onError, payload: event });
         };
 
-        // функция, которая вызывается при получении события от сервера
         socket.onmessage = (event: MessageEvent<any>) => {
           const { data } = event;
           const parsedData = JSON.parse(data);
           const { success, ...restParsedData } = parsedData;
           dispatch({ type: getOrders, payload: restParsedData });
         };
-        // функция, которая вызывается при закрытии соединения
+
         socket.onclose = (event: CloseEvent) => {
           dispatch({ type: onClose, payload: event });
+          socket!.close(); // Закрытие соединения при событии закрытия
+          socket = null;
         };
       }
+
       next(action);
     };
   };
-}; 
+};
