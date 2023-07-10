9216ab9b4eced2da6e6259b10e304092152b8dcd
Stanislav_Kuranov <kuranovstas18@yandex.ru>
2023-07-10 17:08:42 +0500

    исправлен Cypress-тест

--- cypress/integration/BurgerConstructor.spec.js
+++ cypress/integration/BurgerConstructor.spec.js
@@ -8,16 +8,16 @@ describe('BurgerConstructor Drag and Drop', () => {
 
   it('should allow dragging and dropping ingredients', () => {
     // Выбираем ингредиент, который мы будем перетаскивать
-    cy.get('.Ingredient_ingredient__i6v0A').eq(2).as('ingredient');
+    cy.get('[class^="Ingredient_ingredient__"]').eq(2).as('ingredient');
 
     // Выбираем контейнер для перетаскивания (filling)
-    cy.get('.BurgerConstructor_filling__0KjqM').as('fillingContainer');
+    cy.get('[class^="BurgerConstructor_filling__"]').as('fillingContainer');
 
     // Перетаскиваем ингредиент в filling контейнер
     cy.get('@ingredient').trigger('mousedown', { button: 0 });
     cy.get('@fillingContainer').trigger('mousemove').trigger('mouseup', { force: true });
 
     // Проверяем, что ингредиент успешно добавлен в filling контейнер
-    cy.get('.BurgerConstructor_filling__0KjqM').should('have.length', 1);
+    cy.get('[class^="BurgerConstructor_filling__"]').should('have.length', 1);
   });
 });
